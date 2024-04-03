701e77e4365e60f8690cafe6b4e7c6cae2982a23
RuneGSoerensen <eaa23rgso@students.eaaa.dk>
2024-04-03 10:51:35 +0200

    mortenerdum

--- index.js
+++ index.js
@@ -1,1 +1,85 @@
-// Your code here
+
+const dodger = document.querySelector("#dodger");
+const movementSound = document.querySelector("#movementSound");
+const gameOverSound = document.querySelector("#gameoverSound")
+dodger.style.bottom = "100px";
+
+function playGameOverSound() {
+    movementSound.currentTime = 0;
+    gameOverSound.play();
+}
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 10}px`;
+    }
+    if (left <= 1) {
+        playGameOverSound();
+    }
+}
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left < 360) {
+      dodger.style.left = `${left + 10}px`;
+    }
+    if (left >= 360) {
+        playGameOverSound();
+    }
+}
+function moveDodgerUp() {
+    const bottomNumbers = dodger.style.bottom.replace("px", "");
+    const bottom = parseInt(bottomNumbers, 10);
+  
+    if (bottom < 360) {
+      dodger.style.bottom = `${bottom + 10}px`;
+    }
+    if (bottom >= 360) {
+        playGameOverSound();
+    }
+}
+
+function moveDodgerDown() {
+    const bottomNumbers = dodger.style.bottom.replace("px", "");
+    const bottom = parseInt(bottomNumbers, 10);
+  
+    if (bottom > 0) {
+      dodger.style.bottom = `${bottom - 10}px`;
+    }
+    if (bottom < 1) {
+        playGameOverSound();
+    }
+}
+
+function playSoundOnMovement() {
+    movementSound.currentTime = 0;
+    movementSound.play();
+}
+
+
+
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+        playSoundOnMovement();
+    }
+    if (e.key === "ArrowRight") {
+        moveDodgerRight();
+        playSoundOnMovement();
+    }
+    if (e.key === "ArrowUp") {
+        moveDodgerUp();
+        playSoundOnMovement();
+    }
+    if (e.key === "ArrowDown") {
+        moveDodgerDown();
+        playSoundOnMovement();
+    }
+});
+
